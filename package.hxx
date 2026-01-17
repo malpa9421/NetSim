aded442f784da5ad80daf18a1d2993176a896003
Kacper Kiełbasa <kacper.kielbasa5000@gmail.com>
2026-01-17 16:49:29 +0100

    Dodanie package

--- package.hxx
+++ package.hxx
@@ -1,5 +1,22 @@
+#ifndef PACKAGE_PACKAGE_HXX
+#define PACKAGE_PACKAGE_HXX
+
+#include <set>
 #include "types.hxx"
 
 class Package {
-    
+    ElementID id_;
+
+    static std::set<ElementID> used_ids;
+    static std::set<ElementID> freed_ids;
+
+public:
+    Package();
+    Package(ElementID id);
+    Package(Package&& other) noexcept;
+    Package& operator=(Package&& other) noexcept;
+    ~Package();
+    ElementID get_id() const { return id_; }
 };
+
+#endif
--- tests.cpp
+++ tests.cpp
@@ -1,4 +1,5 @@
 #include <gtest/gtest.h>
+#include "package.hxx"
 
 
 TEST(PackageTest, IsAssignedIdLowest) {
