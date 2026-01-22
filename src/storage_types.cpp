e1cee03ba2a3defc98e8413342d5f9e1494605dd
vongraben <vongraben@op.pl>
2026-01-23 00:10:02 +0100

    Working P
    Półprodukty i ich przechowywanie [P] fully working

--- src/storage_types.cpp
+++ src/storage_types.cpp
@@ -1,1 +1,22 @@
 #include "../include/storage_types.hxx"
+
+Package PackageQueue::pop()
+{
+    auto& list = package_list_;
+    Package result;
+
+    bool lifo = (package_queue_type_ == PackageQueueType::LIFO);
+
+    if (lifo)
+    {
+        result = std::move(list.back());
+        list.pop_back();
+    }
+    else
+    {
+        result = std::move(list.front());
+        list.pop_front();
+    }
+
+    return result;
+}
