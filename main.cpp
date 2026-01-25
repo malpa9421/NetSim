87111da1d9997c86b896e7dd1e3fc8f226a24875
Kacper Kiełbasa <kacper.kielbasa5000@gmail.com>
2026-01-25 16:17:28 +0100

    Final Nodes

--- main.cpp
+++ main.cpp
@@ -1,15 +1,5 @@
 #include "gtest/gtest.h"
 
-// karol:
-// ipackagereceiver
-// receiverpeferences
-// packagesender
-
-// moje:
-// ramp
-// worker
-// storehouse
-
 int main(int argc, char** argv) {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
